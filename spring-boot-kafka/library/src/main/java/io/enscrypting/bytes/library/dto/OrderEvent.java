0e440c0adfdbd954234d2ef03748e2dbbfc3b7f0
Ruturaj Mestry <mestryruturaj@gmail.com>
2025-07-28 21:04:12 +0530

    Dev: library | initial dto for events

---
+++ spring-boot-kafka/library/src/main/java/io/enscrypting/bytes/library/dto/OrderEvent.java
@@ -0,0 +1,17 @@
+package io.enscrypting.bytes.library.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrderEvent {
+    private String message;
+    private String status;
+    private Order order;
+}
