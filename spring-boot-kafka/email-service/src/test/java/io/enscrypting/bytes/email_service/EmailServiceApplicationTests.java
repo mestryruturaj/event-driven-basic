30b32b91dd58a268f4e141e353e1f081c8b4c9ae
Ruturaj Mestry <mestryruturaj@gmail.com>
2025-07-28 19:29:45 +0530

    restructured commit;

---
+++ spring-boot-kafka/email-service/src/test/java/io/enscrypting/bytes/email_service/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.enscrypting.bytes.email_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
